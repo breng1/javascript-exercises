2f3fbff79ed7b3e887285279073285dcf4b3b12a
breng1 <brendaknguyen@gmail.com>
2022-09-28 13:55:32 -0700

    Add solution for exercise sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 ) return 'ERROR';
+    if (typeof num1 != 'number' || typeof num2 != 'number') return 'ERROR';
 
+    largeNum = Math.max(num1, num2);
+    smallNum = Math.min(num1, num2)
+    let sum = 0;
+    for (let i = smallNum; i <= largeNum; i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
